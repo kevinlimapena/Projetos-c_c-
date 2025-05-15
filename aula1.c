3255cf9dc07c4b842c0761f2c72afc469ee83b87
Kevin lima pena <kevinlimapena@gmail.com>
2025-05-14 22:29:31 -0400

    first commit

---
+++ aula1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade = 10;
+
+    float altura = 1.75;
+
+    char inicial = 'K';
+
+    char nome[20] = "Kevin";
+
+    printf("Idade %i\n", idade);
+
+    printf("altura %.2f\n", altura);
+
+    printf("Inicial do Nome: %c\n", inicial);
+
+    printf("Nome: %s\n", nome);
+    return 0;
+}
